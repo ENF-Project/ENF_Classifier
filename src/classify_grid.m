68c073a1ac39fde9dadb456d6f9241e3355946fb
Thomas Raynaud de Lage <thomas.raynauddelage@mail.mcgill.ca>
2016-02-20 19:12:10 -0500

    Select only features in classify grids

--- src/classify_grid.m
+++ src/classify_grid.m
@@ -1,5 +1,7 @@
 function classification =  classify_grid(B,x)
-pihat = mnrval(B,x);
+num_features = size(x);
+num_features = num_features(2);
+pihat = mnrval(B,x(:,1:num_features - 1));
 [~,classification] = max(pihat);
 end
 
