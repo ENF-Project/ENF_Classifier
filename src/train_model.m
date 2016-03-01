4d0dbd5e56de458b93a1ee60014d8c02d7bea9ef
Thomas Raynaud de Lage <thomas.raynauddelage@mail.mcgill.ca>
2016-03-01 17:31:26 -0500

    Add standardization

--- src/train_model.m
+++ src/train_model.m
@@ -1,6 +1,11 @@
 function B = train_model(feature_matrix)
 num_features = size(feature_matrix);
 num_features = num_features(2);
+
+feature_matrix(:,1:num_features - 1) = zscore(feature_matrix(:,1:num_features - 1));
+
+
 [B, dev, stats] = mnrfit(feature_matrix(:,1:num_features - 1), feature_matrix(:,num_features));
+ 
 
 end
