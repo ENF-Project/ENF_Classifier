ff974b6c7239d194622bb81b786e8f96c2b1c798
Thomas Raynaud de Lage <thomas.raynauddelage@mail.mcgill.ca>
2016-03-06 17:43:48 -0500

    Add glmfit

--- src/train_model.m
+++ src/train_model.m
@@ -3,14 +3,19 @@ num_features = size(feature_matrix);
 num_samples = num_features(1);
 num_features = num_features(2);
 
-for i = 1 : num_samples
-    
-    feature_matrix(i, 1:num_features - 1) = zscore(feature_matrix(i,1:num_features - 1));
-end
+% for i = 1 : num_samples
+%     
+%     feature_matrix(i, 1:num_features - 1) = zscore(feature_matrix(i,1:num_features - 1));
+% end
 % feature_matrix(:,1:num_features - 1) = zscore(feature_matrix(:,1:num_features - 1));
-feature_matrix
+%feature_matrix
 
 [B, dev, stats] = mnrfit(feature_matrix(:,1:num_features - 1), feature_matrix(:,num_features));
  
+link = @(mu) log(mu ./ (1-mu));
+derlink = @(mu) 1 ./ (mu .* (1-mu));
+invlink = @(resp) 1 ./ (1 + exp(-resp));
+F = {link, derlink, invlink};
+b = glmfit(feature_matrix(:,1:num_features - 1),feature_matrix(:,num_features),'binomial','link',F)
 
 end
